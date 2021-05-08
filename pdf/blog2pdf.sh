f1f513f3762ac9edd9492709bb7df32f78853b05
Giuseppe Barillari <joe2k01dev@gmail.com>
2021-05-08 18:11:27 +0100

    www: Create a LaTeX PDF for the first page and update the pdf script to include more recent posts
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- pdf/blog2pdf.sh
+++ pdf/blog2pdf.sh
@@ -16,13 +16,23 @@ links=( "https://micro-bit-final-project.github.io/groundwork/2020/12/13/groundw
 	"https://micro-bit-final-project.github.io/game/2021/01/23/connection-directions.html"
 	"https://micro-bit-final-project.github.io/game/2021/02/10/initial-coin.html"
 	"https://micro-bit-final-project.github.io/game/2021/02/19/pretty-menu.html"
+	"https://micro-bit-final-project.github.io/game/2021/02/22/idea-generation.html"
+	"https://micro-bit-final-project.github.io/game/2021/02/22/minigame-idea-brainstorming.html"
 	"https://micro-bit-final-project.github.io/game/2021/02/22/overcook-minigame.html"
+	"https://micro-bit-final-project.github.io/game/2021/02/23/game-thought.html"
 	"https://micro-bit-final-project.github.io/game/2021/02/23/match-minigame.html"
 	"https://micro-bit-final-project.github.io/game/2021/03/02/guard-disconnection.html"
 	"https://micro-bit-final-project.github.io/game/2021/03/19/dinorun-minigame.html"
 	"https://micro-bit-final-project.github.io/game/2021/04/05/flashy-li(ves)ghts.html"
+	"https://micro-bit-final-project.github.io/video/2021/04/10/nle-setup.html"
 	"https://micro-bit-final-project.github.io/game/2021/04/13/volume-slider.html"
-	"https://micro-bit-final-project.github.io/game/2021/04/17/credits.html" );
+	"https://micro-bit-final-project.github.io/video/2021/04/13/first-draft-first-half.html"
+	"https://micro-bit-final-project.github.io/video/2021/04/15/voice-recording.html"
+	"https://micro-bit-final-project.github.io/video/2021/04/15/refining-advert.html"
+	"https://micro-bit-final-project.github.io/game/2021/04/17/credits.html"
+	"https://micro-bit-final-project.github.io/game/2021/04/21/which-path.html"
+	"https://micro-bit-final-project.github.io/game/2021/04/24/finishing-off-which-path-minigame.html"
+	"https://micro-bit-final-project.github.io/game/2021/05/07/wheelie-is-back.html"  );
 
 # Iterate through each link
 counter=0
@@ -30,10 +40,10 @@ for i in "${links[@]}"
 do
 	echo "$i";
 	$(wkhtmltopdf $i tmp.pdf);
-	if ((counter==6)) || ((counter==10)) || ((counter==19)); then
+	if ((counter==6)) || ((counter==10)) || ((counter==22)); then
 		$(mv tmp.pdf mu-tmp.pdf);
 	else
-		if ((counter==7)); then
+		if ((counter==7)) || ((counter==25)); then
 			$(mutool poster -y 2 tmp.pdf mu-tmp.pdf);
 		else
 			$(mutool poster -y 5 tmp.pdf mu-tmp.pdf);
@@ -43,6 +53,9 @@ do
 	$(rm tmp.pdf mu-tmp.pdf 2> /dev/null);
 	counter=$((counter+1));
 	sleep 5;
+	if ((counter%10==0)); then
+		sleep 10;
+	fi 
 done
 
 counter=$((counter-1));
@@ -55,8 +68,9 @@ do
                 $(pdfunite $i.pdf tmp.pdf TMP.pdf);
         fi
 
-        if ((i==21)); then
-                $(mv TMP.pdf blog.pdf);
+        if ((i==31)); then
+		$(pdfunite first_page.pdf TMP.pdf blog.pdf);
+                $(rm TMP.pdf 2> /dev/null);
                 $(rm tmp.pdf 2> /dev/null);
         else
                 $(mv TMP.pdf tmp.pdf 2> /dev/null);
